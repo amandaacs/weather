1219f5c9d00c2c4f34e12f59eeda168f7e17e2aa
Paulo Henrique <contactph51@gmail.com>
2026-05-25 01:13:21 -0300

    refactor: remove CidadeController and CidadeIbgeDTO classes

--- src/main/java/com/project/weather/dto/CidadeIbgeDTO.java
+++
@@ -1,7 +0,0 @@
-package com.project.weather.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record CidadeIbgeDTO(String nome) {
-}
